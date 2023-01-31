7ae1332a154beb32a1ac71e99e20aaaaf0aec8a8
Kyle Mathews <mathews.kyle@gmail.com>
2023-01-31 14:51:52 -0800

    Add some initial mutation methods

---
+++ lib/index.test.ts
@@ -0,0 +1,38 @@
+import { beforeAll, assert, expect, test } from "vitest"
+import LMDBArray from "./"
+
+let array: LMDBArray
+
+beforeAll(() => {
+  array = new LMDBArray()
+})
+
+test("push", () => {
+  array.push(12)
+  array.push(13)
+  array.push(14)
+  array.push(10)
+  array.push(989)
+  expect(array.length).eq(5)
+  expect(array.getElementAtIndex(2)).eq(14)
+})
+test(`pop`, () => {
+  const item = array.pop()
+  expect(array.length).eq(4)
+  expect(item).eq(989)
+})
+
+test(`insertAt`, async () => {
+  array.insertAt(456, 2)
+  expect(array.getElementAtIndex(2)).eq(456)
+  expect(array.length).eq(4)
+  array.insertAt(456, 4)
+  expect(array.getElementAtIndex(4)).eq(456)
+  expect(array.length).eq(5)
+})
+
+test(`deleteAt`, () => {
+  array.deleteAt(3)
+  expect(array.length).eq(4)
+  expect(array.getElementAtIndex(3)).toBeUndefined()
+})
---
+++ lib/index.ts
@@ -0,0 +1,66 @@
+declare module "lmdb" {
+  // currently lmdb doesn't have typings for this export
+  export function clearKeptObjects(): void
+}
+
+import { RootDatabase, clearKeptObjects, open } from "lmdb"
+import os from "os"
+import path from "path"
+
+const randomFileName =
+  Math.random().toString(36).substring(2, 15) + "-lmdb-backed-array"
+const filePath = path.join(os.tmpdir(), randomFileName)
+
+class LMDBArray {
+  _nextKey: number
+  db: RootDatabase
+  length: number
+
+  constructor() {
+    this.db = open({
+      path: filePath,
+      compression: true,
+      cache: true,
+    })
+
+    this.length = 0
+    this._nextKey = 0
+  }
+
+  getElementAtIndex(index: number) {
+    return this.db.get(index)
+  }
+  push(element: any) {
+    const index = this._nextKey
+    this.db.put(index, element)
+    this._nextKey++
+    this.length++
+    return this.length
+  }
+  pop(): any {
+    const index: number = this._nextKey - 1
+    const item = this.db.get(index)
+    this.db.remove(index)
+    this._nextKey--
+    this.length--
+    return item
+  }
+  deleteAt(index: number) {
+    const exists = this.db.get(index)
+    this.db.remove(index)
+    if (exists) {
+      this.length--
+    }
+    return this
+  }
+  insertAt(item: any, index: number) {
+    const exists = this.db.get(index)
+    this.db.put(index, item)
+    if (!exists) {
+      this.length++
+    }
+    return this
+  }
+}
+
+export default LMDBArray
